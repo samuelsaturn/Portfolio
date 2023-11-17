5723305c290fe0d853cf9bf02b93b90cf170ec75
saturno <samuelmathias033@gmail.com>
2023-11-16 23:54:33 -0300

    initial commit

---
+++ components/Avatar.js
@@ -0,0 +1,18 @@
+// Next image
+import Image from "next/image";
+
+const Avatar = () => {
+  return (
+    <div className="hidden xl:flex xl:max-w-none">
+      <Image
+        src={"/aa.gif"}
+        width={737}
+        height={678}
+        alt=""
+        className="translate-z-0 w-full h-full"
+      />
+    </div>
+  );
+};
+
+export default Avatar;
---
+++ pages/index.js
@@ -0,0 +1,79 @@
+// Next image
+import image from "next/image";
+
+// Components
+import ParticlesContainer from "../components/ParticlesContainer";
+import ProjectsBtn from "../components/ProjectsBtn";
+import Avatar from "../components/Avatar";
+
+// Framer motion
+import { motion } from "framer-motion";
+
+// Variants
+import { fadeIn } from "../variants";
+
+const Home = () => {
+  return (
+    <div className="bg-primary/60 h-full">
+      {/* texto */}
+      <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
+        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
+          {/* titulo */}
+          <motion.h1
+            variants={fadeIn("down", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h1"
+          >
+            Fazendo coisas <br /> Pra{" "}
+            <span className="text-accent">Ganhar dinheiro</span>
+          </motion.h1>
+          {/* subtitulo */}
+          <motion.p
+            variants={fadeIn("down", 0.3)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
+          >
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the
+          </motion.p>
+          {/* botão */}
+          <div className="flex justify-center xl:hidden relative">
+            <ProjectsBtn />
+          </div>
+          <motion.div
+            variants={fadeIn("down", 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="hidden xl:flex"
+          >
+            <ProjectsBtn />
+          </motion.div>
+        </div>
+      </div>
+      {/* image */}
+      <div className="w-[1200px] h-full absolute right-0 bottom-0">
+        {/* bg img */}
+        <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-center xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
+        {/* Particicles */}
+        <div>Particles</div>
+        {/* Avatar Image */}
+        <motion.div
+          variants={fadeIn("up", 0.5)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          className="w-full h-full max-w-[800px] max-h-[850px] absolute -bottom-32 lg:bottom-0 lg:right-[20%] mix-blend-color-dodge"
+        >
+          <Avatar />
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
