b7f94e1adb715a5605324d70194cf183f0f563af
saturno <samuelmathias033@gmail.com>
2023-12-03 15:16:08 -0300

    fixing bullet swiper pagination

--- pages/projetos/index.js
+++ pages/projetos/index.js
@@ -41,7 +41,7 @@ const Work = () => {
             initial="hidden"
             animate="show"
             exit="hidden"
-            className="w-full xl:max-w-[65%]"
+            className="w-full xl:max-w-[60%]"
           >
             <WorkSlider />
           </motion.div>
